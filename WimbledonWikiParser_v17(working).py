43d76efe1425c64bea07a3db4ea2b329e5abe41c
Mistapepper <bobbysebolao@gmail.com>
2018-10-16 14:13:14 +0100

    Update WimbledonWikiParser_v17(working).py

--- WimbledonWikiParser_v17(working).py
+++ WimbledonWikiParser_v17(working).py
@@ -3,28 +3,22 @@ import requests
 import re 
 import openpyxl
 import os 
-
 year = 1884 
 url = "https://en.wikipedia.org/wiki/{tourneyYear}_Wimbledon_Championships"
-
 wb = openpyxl.load_workbook(os.path.expanduser("~/MyPythonScripts/all_womens_tennis_grand_slam_winners.xlsx"))
 sheet = wb.get_sheet_by_name('Sheet1')
 winnerCol = sheet['D']
 yearCol = sheet['B']
 DOFCol = sheet['A']
 targetRow = 1
-
 for cell in winnerCol:
     response = requests.get(url.format(tourneyYear=year))
     text = response.text
     soup = BeautifulSoup(text, "html.parser")
-
     overviewTable = soup.find('table', attrs ={'class':"infobox vevent"})
-
     if targetRow == 1:
         targetRow += 1
         continue
-
     if overviewTable is None:
         if cell.row == targetRow:
             cell.value = 'None'
@@ -36,28 +30,19 @@ for cell in winnerCol:
             targetRow += 1
             year += 1
             continue 
-
     womensSingles = overviewTable.find('a'[0], title=re.compile ("Women's Singles"))
-
     womensSinglesInfo = womensSingles.parent.parent #this gives you the 'tr' of the 10th 'th' in the 'table'
-
     womensSinglesChampInfo = womensSinglesInfo.next_sibling.next_sibling
-
     womensSinglesChampName = womensSinglesChampInfo.find_all('a')[1].text
-
     if cell.row == targetRow:
         cell.value = womensSinglesChampName
-        wb.save('all_womens_tennis_grand_slam_winners.xlsx')
-            
-    for cell in yearCol:
-        
+        wb.save('all_womens_tennis_grand_slam_winners.xlsx')         
+    for cell in yearCol:    
         tableHeader = overviewTable.find('th')
         womensSinglesChampYear = tableHeader.text[0:4]
-
         if cell.row == targetRow:
             cell.value = womensSinglesChampYear
             wb.save('all_womens_tennis_grand_slam_winners.xlsx')
-
             for cell in DOFCol:
                 if cell.row == targetRow:
                     dateHeader = overviewTable.find('th', attrs={"scope":"row"})
@@ -65,17 +50,13 @@ for cell in winnerCol:
                     nDash = "–"
                     hyphen = "-"
                     words = date.find('td').get_text().split()
-                    print(words)
                     if nDash and hyphen in words:
                         if words.index(nDash) < words.index(hyphen):
                             words.remove(hyphen)
                         else:
-                            words.remove(nDash)
-                            
-                            for index, word in enumerate(words):
-                                
+                            words.remove(nDash)                           
+                            for index, word in enumerate(words):                                
                                 if word == hyphen:
-                                    print(index, word)
                                     if index != 0:
                                         if index != len(words)-1:
                                             before = words[index-1]
@@ -87,7 +68,6 @@ for cell in winnerCol:
                                                 dateYear = (int(year)-1)
                                                 finalDateResult = dateDayStr + ' ' + dateMonth + ' ' + dateYear
                                                 cell.value = finalDateResult
-                                                print(cell.value + '\n')
                                                 wb.save('all_womens_tennis_grand_slam_winners.xlsx')
                                             else: 
                                                 try:
@@ -99,12 +79,9 @@ for cell in winnerCol:
                                                 dateYear = str(year)
                                                 finalDateResult = dateDayStr + ' ' + dateMonth + ' ' + dateYear
                                                 cell.value = finalDateResult
-                                                print(cell.value + '\n')
-                                                wb.save('all_womens_tennis_grand_slam_winners.xlsx')
-                                                   
+                                                wb.save('all_womens_tennis_grand_slam_winners.xlsx')                                                  
                                 else:
                                     if word == nDash:
-                                        print(index, word)
                                         if index != 0:
                                             before = words[index-1]
                                             if index != len(words)-1:
@@ -119,14 +96,10 @@ for cell in winnerCol:
                                                 dateYear = str(year)
                                                 finalDateResult = dateDayStr + ' ' + dateMonth + ' ' + dateYear                          
                                                 cell.value = finalDateResult
-                                                print(cell.value + '\n')
                                                 wb.save('all_womens_tennis_grand_slam_winners.xlsx')
-
                     else:
-                        for index, word in enumerate(words):
-                                
+                        for index, word in enumerate(words):                                
                                 if word == hyphen:
-                                    print(index, word)
                                     if index != 0:
                                         if index != len(words)-1:
                                             before = words[index-1]
@@ -142,12 +115,9 @@ for cell in winnerCol:
                                             dateYear = str(year)
                                             finalDateResult = dateDayStr + ' ' + dateMonth + ' ' + dateYear
                                             cell.value = finalDateResult
-                                            print(cell.value + '\n')
-                                            wb.save('all_womens_tennis_grand_slam_winners.xlsx')
-                                            
+                                            wb.save('all_womens_tennis_grand_slam_winners.xlsx')                                            
                                 else:
                                     if word == nDash:
-                                        print(index, word)
                                         if index != 0:
                                             before = words[index-1]
                                             if index != len(words)-1:
@@ -162,14 +132,8 @@ for cell in winnerCol:
                                                 dateYear = str(year)
                                                 finalDateResult = dateDayStr + ' ' + dateMonth + ' ' + dateYear                          
                                                 cell.value = finalDateResult
-                                                print(cell.value + '\n')
                                                 wb.save('all_womens_tennis_grand_slam_winners.xlsx')    
-
     if year is 1891:
-
         break  # last page
-
     year += 1
     targetRow += 1
-
-
